973e5db79f31157c868673e38d86f9049817dfb9
THIAGO LOPES DOS SANTOS <250692@lti.fatecso.edu.br>
2026-08-25 18:02:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/cadastropessoas/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.cadastropessoas
+import android.os.Bundle
+import android.widget.Adapter
+import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var txtEditnanme: EditText
+    private lateinit var addButton: Button
+    private lateinit var removeButton: Button
+    private lateinit var LVPeople: ListView
+    private val people = mutableListOf<String>();
+    private lateinit var adapter: ArrayAdapter<String>
+    private var Onpeople = -1;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        txtEditnanme = findViewById(R.id.txtnameEdit)
+        addButton = findViewById(R.id.btnAdd)
+        removeButton = findViewById(R.id.removerButton)
+        LVPeople = findViewById(R.id.LVpessoas)
+        people.add("Carlos Enrique")
+        people.add("Thiago Lopes")
+        adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_single_choice,
+            people
+        )
+
+        LVPeople.choiceMode = ListView.CHOICE_MODE_SINGLE
+        LVPeople.adapter = adapter
+
+        LVPeople.setOnItemClickListener { _, _, position, _ ->
+            Onpeople = position
+        }
+        addButton.setOnClickListener {
+
+            val nome = txtEditnanme.text.toString().trim()
+
+            if (nome.isNotEmpty()) {
+                people.add(nome)
+                adapter.notifyDataSetChanged()
+                txtEditnanme.text.clear()
+
+                Toast.makeText(
+                    this,
+                    "Pessoa adicionada!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Digite um nome.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        removeButton.setOnClickListener {
+
+            if (Onpeople != -1) {
+                people.removeAt(Onpeople)
+                Onpeople = -1
+                adapter.notifyDataSetChanged()
+                Toast.makeText(
+                    this,
+                    "Pessoa removida!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Selecione uma pessoa.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+}
